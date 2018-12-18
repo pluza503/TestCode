b709e2654b6d1c92098fca730fe9c2146c17d16b
pluza503 <37098879+pluza503@users.noreply.github.com>
2018-12-18 17:13:06 +0700

    18/12/2

--- TestCode/Form1.vb
+++ TestCode/Form1.vb
@@ -1,22 +1,22 @@
 ﻿Imports System.Data.SqlClient
 Imports System.Data
 Public Class Form1
-    Friend conn As New SqlConnection
+    Friend server As String = "server =KANISORN;initial catalog=test_three;user id=sa;pwd=id523407;"
+    Friend conn As New SqlConnection(server)
     Friend da As New SqlDataAdapter
     Friend ds As New DataSet
     Friend sql As String
     Friend mm As SqlCommand
-    Friend server As String = "server =KANISORN;initial catalog=test_three;user id=sa;pwd=id523407;"
 
     Friend Sub connect()
         If conn.State = ConnectionState.Closed Then conn.Open()
 
     End Sub
     Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        conn()
+
         connect()
         Timer1.Start()
-        Label5.Text = "0"
+
         Label4.Text = server
     End Sub
 
@@ -29,7 +29,7 @@ Public Class Form1
 
         If ds.Tables("datatable").Rows.Count = 0 Then
 
-            sql = "insert into BCISO(SO_ID,SO_PK)values ('" + Label5.Text + "', '" + Label6.Text+"')"
+            sql = "insert into BCISO(SO_PK)values ('" + Label6.Text + "')"
             mm = conn.CreateCommand
             mm.CommandText = sql
             mm.ExecuteNonQuery()
